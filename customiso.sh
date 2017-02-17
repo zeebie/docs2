eebd30bbb5a8bc063212fe98b3c22f70c3bd7444
zeebie <jeong171@gmail.com>
2017-02-17 23:33:58 +0000

    Create customiso.sh

---
+++ customiso.sh
@@ -0,0 +1,7 @@
+sudo gedit /etc/apt/source.list 
+#Edit distro repositories from https://help.ubuntu.com/community/Repositories/CommandLine#
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install aptitude
+sudo aptitude install gdebi-core
